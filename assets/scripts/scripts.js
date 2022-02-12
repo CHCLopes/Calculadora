baa7140b3c439cd2bfed5d074999745eb18fe433
CHCLopes <carlos.sk8r.x@gmail.com>
2022-02-12 10:59:22 -0300

    commit inicial

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,76 @@
+var on = false;
+var dark = false;
+const calcONClass = 'calcON';
+const darkModeClass = 'dark-mode';
+const MODEBTN = document.getElementById('mode');
+const ONOFF = document.getElementById('onoff');
+const DISPLAY = document.getElementById('display');
+const BODY = document.getElementById('body');
+const CALCULATOR = document.getElementById('calculator');
+
+document.getElementById('onoff').innerHTML = "OFF";
+document.getElementById('mode').innerHTML = "OFF";
+
+function darkMode() {
+  if (dark == false && document.getElementById('mode').click){
+    dark = !dark;
+    document.getElementById('mode').innerHTML = "ON";
+    BODY.classList.toggle(darkModeClass);
+    CALCULATOR.classList.toggle(darkModeClass);
+    MODEBTN.classList.toggle(calcONClass)
+  } else if (dark != false && document.getElementById('mode').click){
+    dark = !dark;
+    document.getElementById('mode').textContent = "OFF";
+    BODY.classList.remove(darkModeClass);
+    CALCULATOR.classList.remove(darkModeClass);
+    MODEBTN.classList.remove(calcONClass)
+  }
+}
+
+function onOff(){
+  if (on == false && document.getElementById('onoff').click) {
+    on = !on;
+    document.getElementById('onoff').innerHTML = "ON";
+    ONOFF.classList.toggle(calcONClass);
+    DISPLAY.classList.toggle(calcONClass);
+  } else if (on != false && document.getElementById('onoff').click) {
+    on = !on;
+    document.getElementById('onoff').innerHTML = "OFF";
+    document.getElementById('display').innerHTML = '';
+    ONOFF.classList.remove(calcONClass);
+    DISPLAY.classList.remove(calcONClass);
+    }
+  }
+  
+function clean() {
+  if (on === true) {
+  document.getElementById('display').innerHTML = " ";
+  }
+  return;
+}
+
+function insert(num){
+  if (on === true) {
+        let number = document.getElementById('display').innerHTML;
+        document.getElementById('display').innerHTML = number + num;
+  return;
+  }
+}
+
+function backspace() {
+  if (on === true) {
+    let display = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = display.substring(0, display.length - 1)
+  return;
+  }
+}
+
+function calc(){
+  if (on === true) {
+    let display = document.getElementById('display').innerHTML;
+    if (display){
+      document.getElementById('display').innerHTML = eval(display)
+    }
+  }
+  return;
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+### Projeto Solo 1
+
+# Calculadora
+
+Criando uma calculadora simples usando apenas HTML5, CSS3 e JavaScript.
+Utilizadas técnicas de modificação de DOM e de responsividade.
+
+Sites ferramentas:
+- [icon archive](https://iconarchive.com/) "favicon"
+- [google fonts](fonts.google.com) "fontes online"
+- [netlify](netlify.com) "hospedagem gratuita do site"
+- [dio](web.dio.me) "aprendizado"
+
